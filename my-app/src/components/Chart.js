e78a90dfcfbebbc0bc19f723858088cd98189530
emer289 <murphe65@tcd.ie>
2021-12-29 20:27:25 +0000

    Added transformData to create the x and y axis from the data

--- my-app/src/components/Chart.js
+++ my-app/src/components/Chart.js
@@ -12,14 +12,34 @@ class Chart extends Component {
     componentDidMount () {
         const my_token = process.env.REACT_APP_RAPID_API_KEY
 
-        axios.get('https://api.github.com/users/murphp15/repos', {
+        axios.get('https://api.github.com/users/pippy360/repos', {
             'headers': {
                 'Authorization': `token ${my_token}`
             }
         }).then((res) => {
             const data = res.data
             this.setState(data)
+            console.log(res.data)
         })
+
+    }
+
+    transformData (data) {
+        let plot_data = []
+        let x = []
+        let y = []
+
+        data.map(each => {
+            x.push(each.name)
+            y.push(each.size)
+        })
+
+        plot_data['x'] = x
+        plot_data['y'] = y
+
+        console.log(plot_data)
+
+        return plot_data
 
     }
     
@@ -27,7 +47,7 @@ class Chart extends Component {
     render() {
         return (
             <div>
-
+                
             </div>
         )
     }
