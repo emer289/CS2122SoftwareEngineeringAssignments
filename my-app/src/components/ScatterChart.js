089d81e09ccb0eca4bcc644ed02f83fc76edc707
emer289 <murphe65@tcd.ie>
2021-12-30 20:35:36 +0000

    Await added to improve the display of the data

--- my-app/src/components/ScatterChart.js
+++ my-app/src/components/ScatterChart.js
@@ -1,4 +1,3 @@
-
 import React, {Component} from "react";
 import Plot from 'react-plotly.js'
 import axios from "axios";
@@ -6,60 +5,61 @@ import axios from "axios";
 class ScatterChart extends Component {
 
     state = {
-        following:[],
-        followers:[],
-        data:[]
+        following: [],
+        followers: [],
+        data: []
     }
 
-    componentDidMount () {
+    async componentDidMount() {
         const my_token = process.env.REACT_APP_RAPID_API_KEY
         const followers = []
         const following = []
 
-        axios.get('https://api.github.com/users/pippy360/followers', {
+        const res = await axios.get('https://api.github.com/users/pippy360/followers', {
             'headers': {
                 'Authorization': `token ${my_token}`
             }
-        }).then((res) => {
-            const data = res.data
-            this.setState({data})
-
-            data.map(a => {
-
-                axios.get('https://api.github.com/users/'+ a.login, {
-                    'headers': {
-                        'Authorization': `token ${my_token}`
-                    }
-                }).then((respon) => {
-                    const fol = respon.data
-                    followers.push(fol.followers)
-                    following.push(fol.following)
-
-                })
-
-            })
-            this.setState({following})
-            this.setState({followers})
-            console.log(this.state.following)
-            console.log(this.state.followers)
         })
+        const data = res.data
+        this.setState({data})
+
+        for (const a of data) {
+            const resp = await axios.get('https://api.github.com/users/' + a.login, {
+                'headers': {
+                    'Authorization': `token ${my_token}`
+                }
+            })
+            const fol = resp.data
+            followers.push(fol.followers)
+            following.push(fol.following)
+        }
+
+        this.setState({following, followers})
 
     }
 
 
     render() {
+      
+        if (this.state.followers.length == 0) return null
+
         return (
             <div>
                 <Plot
-                    data = {[
-                        {type: 'scatter',
-                            mode: 'lines',
+                    data={[
+                        {
+                            type: 'scatter',
+                            mode: 'markers',
                             x: this.state.following,
                             y: this.state.followers,
                             marker: {color: 'red'}
                         },
                     ]}
-                    layout = {{width:1000, height: 500, title: 'Followers vs Following'}}
+                    layout={{
+                        width: 1000,
+                        height: 500,
+                        title: 'Followers vs Following' + this.state.followers.length
+                    }}
                 />
 
             </div>
