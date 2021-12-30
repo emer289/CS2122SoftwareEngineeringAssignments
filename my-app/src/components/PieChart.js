2bb091c0e23e79751e640231ad4d5e123f988dd2
emer289 <murphe65@tcd.ie>
2021-12-30 22:24:02 +0000

    Added await to display the data from the API

--- my-app/src/components/PieChart.js
+++ my-app/src/components/PieChart.js
@@ -5,37 +5,36 @@ import axios from "axios";
 class PieChart extends Component {
 
     state = {
+        data: [],
         lang: []
     }
 
-    componentDidMount() {
+    async componentDidMount() {
         const my_token = process.env.REACT_APP_RAPID_API_KEY
+        const lang = []
         //getting the repos
-        axios.get('https://api.github.com/users/murphp15/repos', {
+        const res = await axios.get('https://api.github.com/users/murphp15/repos', {
             'headers': {
                 'Authorization': `token ${my_token}`
             }
-        }).then((res) => {
-            const data = res.data
-            //for each repo getting the languages
-            const lang = []
-            data.map(each => {
-                axios.get('https://api.github.com/repos/murphp15/' + each.name + '/languages', {
-                    'headers': {
-                        'Authorization': `token ${my_token}`
-                    }
-                }).then((response) => {
-
-                    const l = response.data
-                    Object.keys(l).map(a => {
-                            lang.push(a)
-                            this.setState({lang})
-                        }
-                    )
-                })
-            })
-
         })
+        const data = res.data
+        this.setState({data})
+        //for each repo getting the languages
+
+        for (const a of data) {
+            const resp = await axios.get('https://api.github.com/repos/murphp15/' + a.name + '/languages', {
+                'headers': {
+                    'Authorization': `token ${my_token}`
+                }
+            })
+            const l = resp.data
+            Object.keys(l).map(a => {
+                lang.push(a)
+            })
+        }
+        this.setState({lang})
+
 
     }
 
@@ -47,11 +46,12 @@ class PieChart extends Component {
         const map = lang.reduce((acc, e) => acc.set(e, (acc.get(e) || 0) + 1), new Map())
 
 
-        plot_data['v'] = map.values()
-        plot_data['l'] = map.keys()
+        plot_data['v'] = Array.from(map.values())
+        plot_data['l'] = Array.from(map.keys())
 
         console.log(plot_data['v'])
         console.log(plot_data['l'])
+
 
         return plot_data
 
@@ -59,18 +59,18 @@ class PieChart extends Component {
 
 
     render() {
+
+        if(this.state.lang.length < 5) return null
+
         return (
             <div>
                 <Plot
-                    data = {[{
-                        values: this.transformData(this.state.data)['v'],
-                        labels: this.transformData(this.state.data)['l'],
+                    data={[{
+                        values: this.transformData(this.state.lang)['v'],
+                        labels: this.transformData(this.state.lang)['l'],
                         type: 'pie'
                     }]}
-                    layout = {{
-                        height: 400,
-                        width: 500
-                    }}
+                    layout = {{width:1000, height: 500, title: 'Languages Occurancs in Repos'}}
                 />
 
             </div>
