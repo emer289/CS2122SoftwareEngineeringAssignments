bfdee7286c4444c1f1a37a75cf7690275d6f0ea0
emer289 <murphe65@tcd.ie>
2022-01-01 16:10:12 +0000

    adding styling for the search bar and button

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import Form from './components/Form.js';
+import './App.css'
 class App extends Component {
     constructor() {
         super();
