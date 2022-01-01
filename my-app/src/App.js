02b944e5fd4fb65f8a24f8d0e7b235053a4b076c
emer289 <murphe65@tcd.ie>
2022-01-01 15:36:02 +0000

    Using axios to request information from github. This code is taken from https://medium.com/hackernoon/building-github-profile-analytics-using-react-part-1-37e03b0c3366

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import Button from './components/Button.js';
+import axios from 'axios';
 class App extends Component {
     constructor() {
         super();
+        this.state = {
+            username: 'No username',
+            info: ''
+        }
         this.handleClick = this.handleClick.bind(this)
     }
     handleClick(e) {
-        alert("The button was clicked");
+        axios.get('https://api.github.com/users/drastorguev')
+            .then(response => this.setState({
+                username: response.data.login,
+                info : JSON.stringify(response.data, undefined, 2)
+            }));
     }
     render() {
         return (
@@ -18,6 +27,10 @@ class App extends Component {
                     Watch this space...
                 </p>
                 <Button handleClick={this.handleClick}/>
+                <p><b>Username:</b></p>
+                <p>{this.state.username}</p>
+                <b>Information:</b>
+                <pre>{this.state.info}</pre>
             </div>
         );
     }
