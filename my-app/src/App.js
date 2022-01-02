7cf6602e3fde0156548aab5cf9053fa6d3ceed59
emer289 <murphe65@tcd.ie>
2022-01-02 15:16:53 +0000

    App.js updated to use ProfileDetails.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,27 +4,28 @@ import Form from './components/Form.js';
 import Chart from './components/Chart'
 import PirChart from './components/PieChart'
 import ScatterPlot from './components/ScatterChart'
+import ProfileDetails from './components/ProfileDetails.js';
 import './App.css'
+
 class App extends Component {
     constructor() {
         super();
         this.state = {
             gitun: null,
-            info: '',
+            infoclean : '',
             formData: {
                 username: '',
-            }
+            },
         }
         this.handleUserFormSubmit = this.handleUserFormSubmit.bind(this);
         this.handleFormChange= this.handleFormChange.bind(this);
     }
     handleUserFormSubmit(event) {
-        console.log("button pressed", this.state.buttonPressed)
         event.preventDefault();
         axios.get('https://api.github.com/users/'+this.state.formData.username)
             .then(response => this.setState({
                 gitun: response.data.login,
-                info : JSON.stringify(response.data, undefined, 2)
+                infoclean: response.data,
             })).catch((err) => { console.log(err); });
 
     };
@@ -39,6 +40,7 @@ class App extends Component {
             <div>
             <p><b>Username:</b></p>
             <p>{this.state.gitun}</p>
+                <ProfileDetails infoclean={this.state.infoclean}/>
                 <Chart user={this.state.formData.username}/>
                 <PirChart user={this.state.formData.username}/>
                 <ScatterPlot user={this.state.formData.username}/>
@@ -52,11 +54,13 @@ class App extends Component {
                 </header>
                 
                 <div className="app-container">
+
                     <Form
                         formData={this.state.formData}
                         handleUserFormSubmit={this.handleUserFormSubmit}
                         handleFormChange={this.handleFormChange}
                     />
+
                     {this.state.gitun ?
                         this.renderInfo()
                         :
